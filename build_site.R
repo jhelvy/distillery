1c896479b2feb3ae508a79baa0433e44e5ed21eb
John Helveston <john.helveston@gmail.com>
2021-03-19 21:29:29 -0400

    okay that worked, so test 2

--- build_site.R
+++ build_site.R
@@ -3,11 +3,11 @@ source(file.path("R", "functions.R"))
 # First build the footer to capture today's date
 create_footer()
 
-# Then render the README.Rmd file
-rmarkdown::render(
-    input = "README.Rmd",
-    output_file = "README.md",
-    output_format = "github_document")
+# # Then render the README.Rmd file
+# rmarkdown::render(
+#     input = "README.Rmd",
+#     output_file = "README.md",
+#     output_format = "github_document")
 
 # Then render the site
 rmarkdown::render_site(encoding = 'UTF-8')
